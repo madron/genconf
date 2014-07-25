89844b30fa7a5485af7db7ee818b19657ff76c9c
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-07-25 21:45:02 +0200

    added vrf field to LanConfig

--- genconf/config.py
+++ genconf/config.py
@@ -2,9 +2,10 @@ from netaddr import IPNetwork
 
 
 class LanConfig(object):
-    def __init__(self, description='', subnet=None):
+    def __init__(self, description='', subnet=None, vrf=False):
         self.description = description
         self.subnet = subnet
+        self.vrf = vrf
 
     def set_subnet(self, value):
         self._subnet = IPNetwork(value) if value else None
--- genconf/tests/test_config.py
+++ genconf/tests/test_config.py
@@ -4,15 +4,21 @@ from .. import config
 
 
 class LanConfigTest(TestCase):
+    def test_init(self):
+        lan = config.LanConfig(
+            description='Test Lan',
+            subnet='192.168.20.45/24',
+            vrf=True,
+        )
+        self.assertEqual(lan.description, 'Test Lan')
+        self.assertEqual(lan.subnet, IPNetwork('192.168.20.45/24'))
+        self.assertEqual(lan.vrf, True)
+
     def test_init_empty(self):
         lan = config.LanConfig()
         self.assertEqual(lan.description, '')
         self.assertEqual(lan.subnet, None)
-
-    def test_init(self):
-        lan = config.LanConfig(description='Test Lan', subnet='192.168.20.45/24')
-        self.assertEqual(lan.description, 'Test Lan')
-        self.assertEqual(lan.subnet, IPNetwork('192.168.20.45/24'))
+        self.assertEqual(lan.vrf, False)
 
     def test_subnet_set(self):
         lan = config.LanConfig()
