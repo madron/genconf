19e2c97af3c07a0f009b12e5945e2209d347cd5e
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-09-09 19:48:44 +0200

    added PhysicalInterfaceEthernet

--- genconf/hardware.py
+++ genconf/hardware.py
@@ -12,17 +12,11 @@ class PhysicalInterface(object):
         type='ethernet',
         layers=[],
         mtu=1500,
-        duplex='auto',
-        speed='auto',
-        subinterfaces=[]
     ):
         self.name = name
         self.type = type
         self.layers = layers
         self.mtu = mtu
-        self.duplex = duplex
-        self.speed = speed
-        self.subinterfaces = subinterfaces
 
     @property
     def is_layer2(self):
@@ -31,3 +25,20 @@ class PhysicalInterface(object):
     @property
     def is_layer3(self):
         return 3 in self.layers
+
+
+class PhysicalInterfaceEthernet(PhysicalInterface):
+    def __init__(
+        self,
+        duplex='auto',
+        speed='auto',
+        dot1q_mode='access',
+        native_vlan=1,
+        **kwargs
+    ):
+        kwargs['type'] = 'ethernet'
+        super(PhysicalInterfaceEthernet, self).__init__(**kwargs)
+        self.duplex = duplex
+        self.speed = speed
+        self.dot1q_mode = dot1q_mode
+        self.native_vlan = native_vlan
--- genconf/tests/test_hardware.py
+++ genconf/tests/test_hardware.py
@@ -5,23 +5,16 @@ from .. import hardware
 
 class PhysicalInterfaceTest(TestCase):
     def test_init(self):
-        pif = hardware.PhysicalInterface(
-            name='FastEthernet0/1',
-            type='ethernet',
-            layers=[2, 3]
-        )
-        self.assertEqual(pif.name, 'FastEthernet0/1')
-        self.assertEqual(pif.type, 'ethernet')
-        self.assertEqual(pif.layers, [2, 3])
+        pif = hardware.PhysicalInterface(name='Fa0/1', type='atm', layers=[3])
+        self.assertEqual(pif.name, 'Fa0/1')
+        self.assertEqual(pif.type, 'atm')
+        self.assertEqual(pif.layers, [3])
 
     def test_init_empty(self):
         pif = hardware.PhysicalInterface()
         self.assertEqual(pif.name, '')
         self.assertEqual(pif.type, 'ethernet')
         self.assertEqual(pif.layers, [])
-        self.assertEqual(pif.mtu, 1500)
-        self.assertEqual(pif.speed, 'auto')
-        self.assertEqual(pif.subinterfaces, [])
 
     def test_is_layer(self):
         pif = hardware.PhysicalInterface()
@@ -40,3 +33,30 @@ class PhysicalInterfaceTest(TestCase):
         pif.layers = [2]
         self.assertTrue(pif.is_layer2)
         self.assertFalse(pif.is_layer3)
+
+
+class PhysicalInterfaceEthernetTest(TestCase):
+    def test_init(self):
+        pif = hardware.PhysicalInterfaceEthernet(
+            name='FastEthernet0/1',
+            type='will be overwritten',
+            layers=[2, 3],
+            mtu=1460,
+            dot1q_mode='trunk',
+            native_vlan=9
+        )
+        self.assertEqual(pif.name, 'FastEthernet0/1')
+        self.assertEqual(pif.type, 'ethernet')
+        self.assertEqual(pif.layers, [2, 3])
+        self.assertEqual(pif.mtu, 1460)
+        self.assertEqual(pif.speed, 'auto')
+        self.assertEqual(pif.dot1q_mode, 'trunk')
+        self.assertEqual(pif.native_vlan, 9)
+
+    def test_init_empty(self):
+        pif = hardware.PhysicalInterfaceEthernet()
+        self.assertEqual(pif.name, '')
+        self.assertEqual(pif.type, 'ethernet')
+        self.assertEqual(pif.layers, [])
+        self.assertEqual(pif.mtu, 1500)
+        self.assertEqual(pif.speed, 'auto')
