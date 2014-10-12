40cc68886894d7417e5178a0b385a4409347a493
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-10-12 11:49:44 +0200

    renamed json field to configuration

--- genconf/models.py
+++ genconf/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=200)
-    json = models.TextField(blank=True)
+    configuration = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
