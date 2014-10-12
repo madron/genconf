18c75578c66cffb96a41a5836e10f9cce20935dc
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-10-12 11:10:14 +0200

    added few small improvements to Project admin

--- genconf/admin.py
+++ genconf/admin.py
@@ -3,8 +3,8 @@ from django.utils.translation import ugettext_lazy as _
 from . import models
 
 
+@admin.register(models.Project)
 class ProjectAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(models.Project, ProjectAdmin)
+    search_fields = ['name']
+    save_on_top = True
+    save_as = True
--- genconf/models.py
+++ genconf/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=200)
     json = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
