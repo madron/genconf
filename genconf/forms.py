2b3f21a6fceae1bd79668c5978187b00f1b90cad
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-11-28 16:30:24 +0100

    removed utils module from forms

--- genconf/forms.py
+++ genconf/forms.py
@@ -1,23 +1,12 @@
 from django import forms
 from django.utils.translation import ugettext as _
 from . import models
-from . import utils
 
 
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = models.Project
         fields = '__all__'
-
-    def clean_configuration(self):
-        configuration = self.cleaned_data.get('configuration')
-        if not configuration:
-            return ''
-        try:
-            utils.router_load(configuration)
-        except ValueError, exception:
-            raise forms.ValidationError(str(exception))
-        return configuration
 
 
 class ProjectForm1(ProjectForm):
@@ -26,7 +15,6 @@ class ProjectForm1(ProjectForm):
         fields = ['name']
 
 
-class ProjectForm2(ProjectForm):
-    class Meta:
-        model = models.Project
-        fields = ['configuration']
+class ProjectForm2(forms.Form):
+    " This is just a placeholder"
+    pass
