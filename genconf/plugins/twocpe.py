bf11a88656245588f8846c355d01af4cba93e153
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-12-03 19:58:11 +0100

    added draft for get_objects method

--- genconf/plugins/twocpe.py
+++ genconf/plugins/twocpe.py
@@ -17,8 +17,30 @@ class TwoCpe(IPlugin):
         ('wan2', Wan2Form),
     ]
 
+    def save_objects(self, project, objects):
+        from genconf import models
+        old_routers = models.Router.objects.filter(project=project).order_by('pk')
+        for router in objects['routers']:
+            if old_routers.count() > 0:
+                try:
+                    old = old_routers.get(name=router.name)
+                except models.Router.DoesNotExist:
+                    old = old_routers[0]
+                router.pk = old.pk
+            router.save()
+            old_routers = old_routers.exclude(pk=router.pk)
+        if old_routers.count() > 0:
+            old_routers.delete()
+
     def get_objects(self, project, data):
-        pass
+        from genconf import factories
+        return dict(
+            routers=[
+                factories.RouterFactory.build(project=project, name='wan1'),
+                factories.RouterFactory.build(project=project, name='wan2'),
+            ],
+        )
 
     def save(self, project, data):
-        pass
+        objects = self.get_objects(project, data)
+        self.save_objects(project, objects)
--- genconf/tests/test_plugin_twocpe.py
+++ genconf/tests/test_plugin_twocpe.py
@@ -14,6 +14,104 @@ class TwoCpePluginTest(TestCase):
         from ..plugin import manager
         self.plugin_object = manager.getPluginByName('twocpe').plugin_object
 
+    def test_save_objects_new(self):
+        project = factories.ProjectFactory.create(id=1, name='Test')
+        objects = dict(
+            routers=[
+                factories.RouterFactory.build(project=project, name='wan1'),
+                factories.RouterFactory.build(project=project, name='wan2'),
+            ],
+        )
+        self.assertEqual(models.Router.objects.count(), 0)
+        self.plugin_object.save_objects(project, objects)
+        self.assertEqual(models.Router.objects.count(), 2)
+        # wan1
+        router = models.Router.objects.get(name='wan1')
+        self.assertEqual(router.name, 'wan1')
+        self.assertEqual(router.project, project)
+        self.assertEqual(router.project.id, 1)
+        # wan2
+        router = models.Router.objects.get(name='wan2')
+        self.assertEqual(router.name, 'wan2')
+        self.assertEqual(router.project, project)
+        self.assertEqual(router.project.id, 1)
+
+    def test_save_objects_update(self):
+        project = factories.ProjectFactory.create(id=1, name='Test')
+        factories.RouterFactory.create(id=1, name='Wrong', project=project)
+        factories.RouterFactory.create(id=2, name='Old', project=project)
+        objects = dict(
+            routers=[
+                factories.RouterFactory.build(project=project, name='wan1'),
+                factories.RouterFactory.build(project=project, name='wan2'),
+            ],
+        )
+        self.plugin_object.save_objects(project, objects)
+        # wan1
+        router = models.Router.objects.get(name='wan1')
+        self.assertEqual(router.id, 1)
+        self.assertEqual(router.name, 'wan1')
+        self.assertEqual(router.project, project)
+        self.assertEqual(router.project.id, 1)
+        # wan2
+        router = models.Router.objects.get(name='wan2')
+        self.assertEqual(router.id, 2)
+        self.assertEqual(router.name, 'wan2')
+        self.assertEqual(router.project, project)
+        self.assertEqual(router.project.id, 1)
+
+    def test_save_objects_update_one_more(self):
+        project = factories.ProjectFactory.create(id=1, name='Test')
+        factories.RouterFactory.create(id=1, name='Wrong', project=project)
+        factories.RouterFactory.create(id=2, name='Old', project=project)
+        factories.RouterFactory.create(id=3, name='One more', project=project)
+        objects = dict(
+            routers=[
+                factories.RouterFactory.build(project=project, name='wan1'),
+                factories.RouterFactory.build(project=project, name='wan2'),
+            ],
+        )
+        self.assertEqual(models.Router.objects.count(), 3)
+        self.plugin_object.save_objects(project, objects)
+        self.assertEqual(models.Router.objects.count(), 2)
+        # wan1
+        router = models.Router.objects.get(name='wan1')
+        self.assertEqual(router.id, 1)
+        self.assertEqual(router.name, 'wan1')
+        self.assertEqual(router.project, project)
+        self.assertEqual(router.project.id, 1)
+        # wan2
+        router = models.Router.objects.get(name='wan2')
+        self.assertEqual(router.id, 2)
+        self.assertEqual(router.name, 'wan2')
+        self.assertEqual(router.project, project)
+        self.assertEqual(router.project.id, 1)
+
+    def test_save_objects_update_one_less(self):
+        project = factories.ProjectFactory.create(id=1, name='Test')
+        factories.RouterFactory.create(id=1, name='Wrong', project=project)
+        objects = dict(
+            routers=[
+                factories.RouterFactory.build(project=project, name='wan1'),
+                factories.RouterFactory.build(project=project, name='wan2'),
+            ],
+        )
+        self.assertEqual(models.Router.objects.count(), 1)
+        self.plugin_object.save_objects(project, objects)
+        self.assertEqual(models.Router.objects.count(), 2)
+        # wan1
+        router = models.Router.objects.get(name='wan1')
+        self.assertEqual(router.id, 1)
+        self.assertEqual(router.name, 'wan1')
+        self.assertEqual(router.project, project)
+        self.assertEqual(router.project.id, 1)
+        # wan2
+        router = models.Router.objects.get(name='wan2')
+        self.assertEqual(router.id, 2)
+        self.assertEqual(router.name, 'wan2')
+        self.assertEqual(router.project, project)
+        self.assertEqual(router.project.id, 1)
+
     def test_get_objects(self):
         project = factories.ProjectFactory(name='Test')
         data = dict(
@@ -21,3 +119,11 @@ class TwoCpePluginTest(TestCase):
             wan2=dict(access_type='adsl'),
         )
         objects = self.plugin_object.get_objects(project, data)
+        # Router wan1
+        router = objects['routers'][0]
+        self.assertEqual(router.project, project)
+        self.assertEqual(router.name, 'wan1')
+        # Router wan2
+        router = objects['routers'][1]
+        self.assertEqual(router.project, project)
+        self.assertEqual(router.name, 'wan2')
