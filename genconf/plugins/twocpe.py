cd6ebc4c4a9e5946848e693d54f6b060bb3c0a26
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-12-04 17:02:24 +0100

    revactored twocpe save_objects method

--- genconf/plugins/twocpe.py
+++ genconf/plugins/twocpe.py
@@ -5,10 +5,11 @@ from genconf import constants
 
 class Wan1Form(forms.Form):
     access_type = forms.ChoiceField(choices=constants.ACCESS_TYPE_CHOICES)
-
+    router = forms.ChoiceField(choices=constants.ROUTER_TYPE_CHOICES)
 
 class Wan2Form(forms.Form):
     access_type = forms.ChoiceField(choices=constants.ACCESS_TYPE_CHOICES)
+    router = forms.ChoiceField(choices=constants.ROUTER_TYPE_CHOICES)
 
 
 class TwoCpe(IPlugin):
@@ -17,30 +18,60 @@ class TwoCpe(IPlugin):
         ('wan2', Wan2Form),
     ]
 
-    def save_objects(self, project, objects):
+    def save_objects(self, objects):
         from genconf import models
-        old_routers = models.Router.objects.filter(project=project).order_by('pk')
-        for router in objects['routers']:
-            if old_routers.count() > 0:
-                try:
-                    old = old_routers.get(name=router.name)
-                except models.Router.DoesNotExist:
-                    old = old_routers[0]
-                router.pk = old.pk
-            router.save()
-            old_routers = old_routers.exclude(pk=router.pk)
-        if old_routers.count() > 0:
-            old_routers.delete()
+        from genconf.utils import save_instances
+        save_instances(
+            objects.get('router', []),
+            fixed_fields=['project'],
+            search_fields=['name']
+        )
+        save_instances(
+            objects.get('vlan', []),
+            fixed_fields=['router'],
+            search_fields=['tag']
+        )
+        save_instances(
+            objects.get('physicalinterface', []),
+            fixed_fields=['router'],
+            search_fields=['name']
+        )
 
     def get_objects(self, project, data):
         from genconf import factories
-        return dict(
-            routers=[
-                factories.RouterFactory.build(project=project, name='wan1'),
-                factories.RouterFactory.build(project=project, name='wan2'),
-            ],
+        from genconf import hardware
+        objects = dict(
+            router=[],
+            vlan=[],
+            physicalinterface=[],
         )
+        for name, wan in [('wan1', data['wan1']), ('wan2', data['wan2'])]:
+            # Router
+            router = factories.RouterFactory.build(
+                project=project,
+                name=name,
+                model=wan['router']
+            )
+            objects['router'].append(router)
+            # Vlan
+            vlan = factories.VlanFactory.build(
+                router=router,
+                tag=1,
+            )
+            objects['vlan'].append(vlan)
+            # PhysicalInterface
+            router_type = hardware.ROUTER_TYPE[router.model]
+            for interface in router_type['interfaces']:
+                pif = factories.PhysicalInterfaceFactory.build(
+                    router=router,
+                    name=interface['name'],
+                    type=interface['type'],
+                    layer=interface['layer'],
+                    native_vlan=vlan,
+                )
+                objects['physicalinterface'].append(pif)
+        return objects
 
     def save(self, project, data):
         objects = self.get_objects(project, data)
-        self.save_objects(project, objects)
+        self.save_objects(objects)
--- genconf/tests/test_plugin_twocpe.py
+++ genconf/tests/test_plugin_twocpe.py
@@ -1,29 +1,100 @@
 from django.test import TestCase
 from .. import factories
 from .. import models
+from ..plugins import twocpe
 
 
-class ProjectModelTest(TestCase):
-    def test_str(self):
-        project = factories.ProjectFactory(name='Test')
-        self.assertEqual(str(project), 'Test')
+class GetObjectsTest(TestCase):
+    def setUp(self):
+        from ..plugin import manager
+        self.plugin_object = manager.getPluginByName('twocpe').plugin_object
+        self.project = factories.ProjectFactory(name='Test')
+
+    def test_get_objects(self):
+        data = dict(
+            wan1=dict(access_type='ethernet', router='c1841'),
+            wan2=dict(access_type='adsl', router='c1801'),
+        )
+        objects = self.plugin_object.get_objects(self.project, data)
+        #
+        # Router wan1
+        #
+        router = objects['router'][0]
+        self.assertEqual(router.project, self.project)
+        self.assertEqual(router.name, 'wan1')
+        self.assertEqual(router.model, 'c1841')
+        # Vlan
+        vlan = objects['vlan'][0]
+        self.assertEqual(vlan.tag, 1)
+        self.assertEqual(vlan.router, router)
+        # PhysicalInterface
+        interface = objects['physicalinterface'][0]
+        self.assertEqual(interface.router, router)
+        self.assertEqual(interface.name, 'fe0/0')
+        self.assertEqual(interface.type, 'ethernet')
+        self.assertEqual(interface.layer, 3)
+        interface = objects['physicalinterface'][1]
+        self.assertEqual(interface.router, router)
+        self.assertEqual(interface.name, 'fe0/1')
+        self.assertEqual(interface.type, 'ethernet')
+        self.assertEqual(interface.layer, 3)
+        #
+        # Router wan2
+        #
+        router = objects['router'][1]
+        self.assertEqual(router.project, self.project)
+        self.assertEqual(router.name, 'wan2')
+        self.assertEqual(router.model, 'c1801')
+        # Vlan
+        vlan = objects['vlan'][1]
+        self.assertEqual(vlan.tag, 1)
+        self.assertEqual(vlan.router, router)
+        # PhisicalInterface
+        interface = objects['physicalinterface'][2]
+        self.assertEqual(interface.router, router)
+        self.assertEqual(interface.name, 'atm0')
+        self.assertEqual(interface.type, 'atm')
+        self.assertEqual(interface.layer, 3)
+        interface = objects['physicalinterface'][3]
+        self.assertEqual(interface.router, router)
+        self.assertEqual(interface.name, 'fe0')
+        self.assertEqual(interface.type, 'ethernet')
+        self.assertEqual(interface.layer, 3)
+        interface = objects['physicalinterface'][11]
+        self.assertEqual(interface.router, router)
+        self.assertEqual(interface.name, 'fe8')
+        self.assertEqual(interface.type, 'ethernet')
+        self.assertEqual(interface.layer, 2)
 
 
-class TwoCpePluginTest(TestCase):
+class SaveObjectsTest(TestCase):
     def setUp(self):
         from ..plugin import manager
         self.plugin_object = manager.getPluginByName('twocpe').plugin_object
 
-    def test_save_objects_new(self):
+    def test_all_models(self):
+        project = factories.ProjectFactory.create(name='Test')
+        router = factories.RouterFactory.build(project=project)
+        vlan = factories.VlanFactory.build(router=router, tag=20)
+        objects = dict(
+            router=[router],
+            vlan=[vlan],
+        )
+        self.plugin_object.save_objects(objects)
+        self.assertEqual(models.Project.objects.count(), 1)
+        self.assertEqual(models.Router.objects.count(), 1)
+        self.assertEqual(models.Vlan.objects.count(), 1)
+
+    def test_new(self):
         project = factories.ProjectFactory.create(id=1, name='Test')
         objects = dict(
-            routers=[
+            router=[
                 factories.RouterFactory.build(project=project, name='wan1'),
                 factories.RouterFactory.build(project=project, name='wan2'),
             ],
         )
         self.assertEqual(models.Router.objects.count(), 0)
-        self.plugin_object.save_objects(project, objects)
+        self.plugin_object.save_objects(objects)
         self.assertEqual(models.Router.objects.count(), 2)
         # wan1
         router = models.Router.objects.get(name='wan1')
@@ -36,17 +107,36 @@ class TwoCpePluginTest(TestCase):
         self.assertEqual(router.project, project)
         self.assertEqual(router.project.id, 1)
 
-    def test_save_objects_update(self):
+    def test_1_existing(self):
+        project = factories.ProjectFactory.create(id=1, name='Test')
+        router = factories.RouterFactory.build(id=1, project=project)
+        factories.VlanFactory.create(router=router, tag=20)
+        objects = dict(
+            vlan=[
+                factories.VlanFactory.build(router=router, tag=1),
+                factories.VlanFactory.build(router=router, tag=2),
+            ],
+        )
+        self.assertEqual(models.Vlan.objects.count(), 1)
+        self.plugin_object.save_objects(objects)
+        self.assertEqual(models.Vlan.objects.count(), 2)
+        # vlan
+        vlan = models.Vlan.objects.get(tag=1)
+        self.assertEqual(vlan.router.id, 1)
+        vlan = models.Vlan.objects.get(tag=2)
+        self.assertEqual(vlan.router.id, 1)
+
+    def test_2_existing(self):
         project = factories.ProjectFactory.create(id=1, name='Test')
         factories.RouterFactory.create(id=1, name='Wrong', project=project)
         factories.RouterFactory.create(id=2, name='Old', project=project)
         objects = dict(
-            routers=[
+            router=[
                 factories.RouterFactory.build(project=project, name='wan1'),
                 factories.RouterFactory.build(project=project, name='wan2'),
             ],
         )
-        self.plugin_object.save_objects(project, objects)
+        self.plugin_object.save_objects(objects)
         # wan1
         router = models.Router.objects.get(name='wan1')
         self.assertEqual(router.id, 1)
@@ -60,19 +150,19 @@ class TwoCpePluginTest(TestCase):
         self.assertEqual(router.project, project)
         self.assertEqual(router.project.id, 1)
 
-    def test_save_objects_update_one_more(self):
+    def test_3_existing(self):
         project = factories.ProjectFactory.create(id=1, name='Test')
         factories.RouterFactory.create(id=1, name='Wrong', project=project)
         factories.RouterFactory.create(id=2, name='Old', project=project)
         factories.RouterFactory.create(id=3, name='One more', project=project)
         objects = dict(
-            routers=[
+            router=[
                 factories.RouterFactory.build(project=project, name='wan1'),
                 factories.RouterFactory.build(project=project, name='wan2'),
             ],
         )
         self.assertEqual(models.Router.objects.count(), 3)
-        self.plugin_object.save_objects(project, objects)
+        self.plugin_object.save_objects(objects)
         self.assertEqual(models.Router.objects.count(), 2)
         # wan1
         router = models.Router.objects.get(name='wan1')
@@ -87,43 +177,19 @@ class TwoCpePluginTest(TestCase):
         self.assertEqual(router.project, project)
         self.assertEqual(router.project.id, 1)
 
-    def test_save_objects_update_one_less(self):
-        project = factories.ProjectFactory.create(id=1, name='Test')
-        factories.RouterFactory.create(id=1, name='Wrong', project=project)
-        objects = dict(
-            routers=[
-                factories.RouterFactory.build(project=project, name='wan1'),
-                factories.RouterFactory.build(project=project, name='wan2'),
-            ],
-        )
-        self.assertEqual(models.Router.objects.count(), 1)
-        self.plugin_object.save_objects(project, objects)
-        self.assertEqual(models.Router.objects.count(), 2)
-        # wan1
-        router = models.Router.objects.get(name='wan1')
-        self.assertEqual(router.id, 1)
-        self.assertEqual(router.name, 'wan1')
-        self.assertEqual(router.project, project)
-        self.assertEqual(router.project.id, 1)
-        # wan2
-        router = models.Router.objects.get(name='wan2')
-        self.assertEqual(router.id, 2)
-        self.assertEqual(router.name, 'wan2')
-        self.assertEqual(router.project, project)
-        self.assertEqual(router.project.id, 1)
 
-    def test_get_objects(self):
-        project = factories.ProjectFactory(name='Test')
+class SaveTest(TestCase):
+    def setUp(self):
+        from ..plugin import manager
+        self.plugin_object = manager.getPluginByName('twocpe').plugin_object
+
+    def test_save(self):
+        project = factories.ProjectFactory.create(name='Test')
         data = dict(
-            wan1=dict(access_type='ethernet'),
-            wan2=dict(access_type='adsl'),
+            wan1=dict(access_type='ethernet', router='c1841'),
+            wan2=dict(access_type='adsl', router='c1801'),
         )
-        objects = self.plugin_object.get_objects(project, data)
-        # Router wan1
-        router = objects['routers'][0]
-        self.assertEqual(router.project, project)
-        self.assertEqual(router.name, 'wan1')
-        # Router wan2
-        router = objects['routers'][1]
-        self.assertEqual(router.project, project)
-        self.assertEqual(router.name, 'wan2')
+        self.plugin_object.save(project, data)
+        self.assertEqual(models.Project.objects.count(), 1)
+        self.assertEqual(models.Router.objects.count(), 2)
+
