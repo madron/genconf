efd51861fd07cb9258a3d0a63c50ae80255ac86d
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-12-11 18:24:46 +0100

    added django_nose app to test settings

--- settings/settings_test.py
+++ settings/settings_test.py
@@ -1,1 +1,16 @@
 from .settings_common import *
+
+INSTALLED_APPS = list(INSTALLED_APPS)
+INSTALLED_APPS.append('django_nose')
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    # '--with-coverage',
+    # '--cover-html',
+    '--cover-branches',
+    '--cover-erase',
+    '--cover-package=genconf,dictserializer',
+    '--cover-inclusive',
+    '--cover-html-dir=coverage',
+]
