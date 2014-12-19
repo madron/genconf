1a96aff17c477efc8282bb5c5d2c7f324a4fc266
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-12-19 10:01:54 +0100

    added NetIPNetworkField model field to netutils

--- netutils/modelfields.py
+++ netutils/modelfields.py
@@ -1,5 +1,5 @@
 from django.db.models.fields import GenericIPAddressField
-from netaddr import IPAddress
+from netaddr import IPAddress, IPNetwork
 from . import formfields
 
 
@@ -21,3 +21,23 @@ class NetIPAddressField(GenericIPAddressField):
         defaults = dict(form_class=formfields.NetIPAddressField)
         defaults.update(kwargs)
         return super(NetIPAddressField, self).formfield(**defaults)
+
+
+class NetIPNetworkField(GenericIPAddressField):
+    description = 'netaddr.IPNetwork'
+
+    def to_python(self, value):
+        if isinstance(value, IPNetwork) or value is None:
+            return value
+        return IPNetwork(value)
+
+    def get_prep_value(self, value):
+        ' catches value right before sending to db '
+        if value:
+            return str(value)
+        return None
+
+    def formfield(self, **kwargs):
+        defaults = dict(form_class=formfields.NetIPNetworkField)
+        defaults.update(kwargs)
+        return super(NetIPNetworkField, self).formfield(**defaults)
--- netutils/tests/test_modelfields.py
+++ netutils/tests/test_modelfields.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from netaddr import IPAddress
+from netaddr import IPAddress, IPNetwork
 from .. import formfields
 from .. import modelfields
 
@@ -21,3 +21,23 @@ class NetIPAddressFieldTest(TestCase):
         f = modelfields.NetIPAddressField().formfield()
         self.assertIsInstance(f, formfields.NetIPAddressField)
         self.assertEqual(f.clean('127.0.0.1'), IPAddress('127.0.0.1'))
+
+
+class NetIPNetworkFieldTest(TestCase):
+    def test_to_python(self):
+        f = modelfields.NetIPNetworkField()
+        self.assertEqual(f.to_python(None), None)
+        self.assertEqual(f.to_python('0.0.0.0/0'), IPNetwork('0.0.0.0/0'))
+        self.assertEqual(f.to_python('127.0.0.1/32'), IPNetwork('127.0.0.1/32'))
+        self.assertEqual(f.to_python('192.168.1.1/30'), IPNetwork('192.168.1.1/30'))
+
+    def test_get_prep_value(self):
+        f = modelfields.NetIPNetworkField()
+        self.assertEqual(f.get_prep_value(''), None)
+        self.assertEqual(f.get_prep_value(None), None)
+        self.assertEqual(f.get_prep_value(IPNetwork('172.16.0.0/16')), '172.16.0.0/16')
+
+    def test_formfield(self):
+        f = modelfields.NetIPNetworkField().formfield()
+        self.assertIsInstance(f, formfields.NetIPNetworkField)
+        self.assertEqual(f.clean('192.168.1.0/24'), IPNetwork('192.168.1.0/24'))
