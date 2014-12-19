c839f6f412e23456419cc68f917aa8c8f1d03a15
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2014-12-19 10:52:12 +0100

    fixed NetIPNetworkField model field

--- netutils/modelfields.py
+++ netutils/modelfields.py
@@ -1,4 +1,5 @@
-from django.db.models.fields import GenericIPAddressField
+from django.core import checks
+from django.db.models.fields import CharField, GenericIPAddressField
 from netaddr import IPAddress, IPNetwork
 from . import formfields
 
@@ -23,8 +24,35 @@ class NetIPAddressField(GenericIPAddressField):
         return super(NetIPAddressField, self).formfield(**defaults)
 
 
-class NetIPNetworkField(GenericIPAddressField):
+class NetIPNetworkField(CharField):
     description = 'netaddr.IPNetwork'
+
+    def __init__(self, protocol='both', *args, **kwargs):
+        kwargs['max_length'] = 43
+        self.protocol = protocol
+        super(NetIPNetworkField, self).__init__(*args, **kwargs)
+        self.validators = []
+
+    def _check_blank_and_null_values(self, **kwargs):
+        if not getattr(self, 'null', False) and getattr(self, 'blank', False):
+            return [
+                checks.Error(
+                    ('NetIPNetworkField cannot have blank=True if null=False, '
+                     'as blank values are stored as nulls.'),
+                    hint=None,
+                    obj=self,
+                    id='fields.E150',
+                )
+            ]
+        return []
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(NetIPNetworkField, self).deconstruct()
+        if not self.protocol == 'both':
+            kwargs['protocol'] = self.protocol
+        if kwargs.get("max_length", None) == 43:
+            del kwargs['max_length']
+        return name, path, args, kwargs
 
     def to_python(self, value):
         if isinstance(value, IPNetwork) or value is None:
