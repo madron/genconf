019357905d1228e544b646ba83d9fc9746949ca7
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2015-01-10 11:39:03 +0100

    added ordering to netcore models

--- netcore/models.py
+++ netcore/models.py
@@ -3,11 +3,12 @@ from netutils.modelfields import NetIPAddressField
 
 
 class Bras(models.Model):
-    name = models.CharField(max_length=200, db_index=True)
+    name = models.CharField(max_length=200, db_index=True, unique=True)
     management_ip = NetIPAddressField(blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'bras'
+        ordering = ['name']
 
     def __str__(self):
         return self.name
@@ -15,11 +16,20 @@ class Bras(models.Model):
 
 class Vrf(models.Model):
     bras = models.ForeignKey(Bras)
-    number = models.IntegerField(db_index=True, unique=True)
-    name = models.CharField(max_length=200, db_index=True, unique=True)
+    number = models.IntegerField(db_index=True)
+    name = models.CharField(max_length=200, db_index=True)
 
     class Meta:
         verbose_name_plural = 'vrf'
+        ordering = ['bras__name', 'number', 'name']
+        unique_together = [
+            ['bras', 'number'],
+            ['bras', 'name'],
+        ]
+        index_together = [
+            ['bras', 'number', 'name'],
+            ['bras', 'name'],
+        ]
 
     def __str__(self):
         return '%d %s (%s)' % (self.number, self.name, self.bras)
