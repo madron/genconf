6f25c67a22ba3efba804248ab1116483ce3f3f0a
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2015-01-10 11:05:10 +0100

    added netcore Bras fixtures

--- reset_test_data.sh
+++ reset_test_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 rm db.sqlite3
 ./manage.py migrate --noinput --verbosity=1
-./manage.py loaddata auth_admin genconf_test.json
+./manage.py loaddata auth_admin netcore_test.json genconf_test.json
