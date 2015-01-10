6a5829c6098ceae5f91c7c62835d63892235b19d
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2015-01-10 11:40:26 +0100

    added netcore VrfAdmin

--- netcore/admin.py
+++ netcore/admin.py
@@ -5,3 +5,11 @@ from . import models
 @admin.register(models.Bras)
 class BrasAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(models.Vrf)
+class VrfAdmin(admin.ModelAdmin):
+    list_display = ('bras', 'number', 'name')
+    list_display_links = ('number', 'name')
+    search_fields = ('number', 'name')
+    list_filter = ('bras',)
--- netcore/tests/test_admin.py
+++ netcore/tests/test_admin.py
@@ -34,3 +34,36 @@ class BrasAdminTest(TestCase):
         url = reverse('admin:netcore_bras_delete', args=(obj.pk,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+
+class VrfAdminTest(TestCase):
+    def setUp(self):
+        factories.UserFactory()
+        self.assertTrue(self.client.login(username='test', password='pass'))
+        self.list = reverse('admin:netcore_vrf_changelist')
+
+    def test_list(self):
+        response = self.client.get(self.list)
+        self.assertEqual(response.status_code, 200)
+
+    def test_search(self):
+        data = dict(q='text')
+        response = self.client.get(self.list, data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_add(self):
+        url = reverse('admin:netcore_vrf_add')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail(self):
+        obj = factories.VrfFactory(number=1)
+        url = reverse('admin:netcore_vrf_change', args=(obj.pk,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete(self):
+        obj = factories.VrfFactory(number=1)
+        url = reverse('admin:netcore_vrf_delete', args=(obj.pk,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
