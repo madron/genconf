0ea36aeec9ed3a235922eaf3bd133c3bd3d97117
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2015-01-10 11:01:20 +0100

    added draft for BrasAdmin

--- netcore/tests/factories.py
+++ netcore/tests/factories.py
@@ -1,5 +1,19 @@
 import factory
+from django.contrib.auth.models import User
 from .. import models
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    FACTORY_FOR = User
+    FACTORY_DJANGO_GET_OR_CREATE = ('username',)
+
+    username = 'test'
+    is_active = True
+    is_superuser = True
+    is_staff = True
+    # 'pass'
+    password = 'pbkdf2_sha256$12000$LWhlUQyAntYP$FtxgZ9CnZBTbrvcjHJO6StuAJoQqMRDRTFXzYtxRRhg='
+    email = 'test@example.com'
 
 
 class BrasFactory(factory.django.DjangoModelFactory):
---
+++ netcore/tests/test_admin.py
@@ -0,0 +1,36 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from . import factories
+
+
+class BrasAdminTest(TestCase):
+    def setUp(self):
+        factories.UserFactory()
+        self.assertTrue(self.client.login(username='test', password='pass'))
+        self.list = reverse('admin:netcore_bras_changelist')
+
+    def test_list(self):
+        response = self.client.get(self.list)
+        self.assertEqual(response.status_code, 200)
+
+    def test_search(self):
+        data = dict(q='text')
+        response = self.client.get(self.list, data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_add(self):
+        url = reverse('admin:netcore_bras_add')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail(self):
+        obj = factories.BrasFactory(name='Bras name')
+        url = reverse('admin:netcore_bras_change', args=(obj.pk,))
+        response = self.client.get(url)
+        self.assertContains(response, 'value="Bras name"')
+
+    def test_delete(self):
+        obj = factories.BrasFactory()
+        url = reverse('admin:netcore_bras_delete', args=(obj.pk,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
